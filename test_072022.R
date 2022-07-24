f001e5b2f5569626216682e16da37354b3d2d66e
Dongho (Tom) Choi <89372312+tomchoi91@users.noreply.github.com>
2022-07-23 21:23:33 -0500

    testing update

--- test_072022.R
+++ test_072022.R
@@ -16,6 +16,7 @@ m.out
 #Checking balance before and after matching:
 summary(m.out)
 
+<<<<<<< Updated upstream
 install.packages("gtsummary")
 install.packages("rlang")
 library(gtsummary)
@@ -24,3 +25,7 @@ trial2 <- trial %>% select(trt, age, grade)
 trial2 %>% tbl_summary()
 sum(is.na(trial2$age))
 trial2 %>% tbl_summary(by = trt) %>% add_p()
+=======
+
+
+>>>>>>> Stashed changes
